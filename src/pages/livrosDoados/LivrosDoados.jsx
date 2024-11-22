804c441437275c7748972547e647adef43d6a020
EnzoFabriciok <pauloeenzo33@gmail.com>
2024-11-22 19:04:38 -0300

    first commit

---
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -0,0 +1,5 @@
+export default function LivrosDoados() {
+    return(
+        <h1>Eu sou Livros Doados</h1>
+    )
+}
---
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -0,0 +1,5 @@
+export default function QueroDoar() {
+    return(
+        <h1>Eu sou Quero Doar</h1>
+    )
+}
